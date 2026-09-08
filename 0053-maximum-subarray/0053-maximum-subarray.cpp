0cee1d34a2e70b9d8d32e544917b58d043baf5c1
Om kumar <omkumar1.848@gmail.com>
2026-09-08 05:38:34 +0530

    Time: 35 ms (4.49%), Space: 72.8 MB (14.9%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int max_sum = INT_MIN, curr_sum = 0;
-        for(int i = 0;i<nums.size();i++){
-            curr_sum += nums[i];
-            max_sum = max(curr_sum,max_sum);
-            if(curr_sum<0) curr_sum = 0;
+    int solve(vector<int> &nums, int l , int r){
+        if(l==r) return nums[l];
+        int mid = l + (r-l)/2;
+        int left_s = solve(nums,l,mid);
+        int right_s = solve(nums,mid +1, r);
+
+        int left_sum = INT_MIN;
+        int sum =0;
+        for(int i =mid;i>=l;i--){
+            sum += nums[i];
+            left_sum = max(sum, left_sum);
         }
-        return max_sum;
+
+        int right_sum = INT_MIN;
+        sum =0;
+        for(int i =mid+1;i<=r;i++){
+            sum += nums[i];
+            right_sum = max(sum, right_sum);
+        }
+
+        int both_s_sum = left_sum + right_sum;
+        return max({left_s,right_s,both_s_sum});
+    }
+    int maxSubArray(vector<int>& nums) {
+        return solve(nums,0,nums.size()-1);
     }
 };
